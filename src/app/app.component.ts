e327db493cb796e2f2f6fd67fe61bdcf57939e36
danilotiago <programacao.desenvolvimento@gmail.com>
2019-07-13 00:46:04 -0300

    iterando sobre as photos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  photos: Array<any> = [
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/439px-Microsoft_logo.svg.png',
+      description: 'Microsoft'
+    },
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/439px-Microsoft_logo.svg.png',
+      description: 'Microsoft'
+    }
+  ];
 }
