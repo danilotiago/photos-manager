aee677d3456ec639e5a39ffdef83c162c7abe438
danilotiago <programacao.desenvolvimento@gmail.com>
2019-07-14 19:32:34 -0300

    passando parametros via rota

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-photo-list',
@@ -12,11 +13,16 @@ export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
 
-  constructor(private photoService: PhotoService) { }
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute
+    ) { }
 
   ngOnInit(): void {
+    const username = this.activatedRoute.snapshot.params.username;
+
     this.photoService
-      .listFromUser('flavio')
+      .listFromUser(username)
       .subscribe(photos => this.photos = photos)
   }
 
