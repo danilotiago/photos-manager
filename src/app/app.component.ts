45df4d67dc4a5880a1570eb91ac84ce66f8ae53a
danilotiago <programacao.desenvolvimento@gmail.com>
2019-07-13 23:03:46 -0300

    importando httpclient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,14 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  photos: Array<any> = [
-    {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/439px-Microsoft_logo.svg.png',
-      description: 'Microsoft'
-    },
-    {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/439px-Microsoft_logo.svg.png',
-      description: 'Microsoft'
-    }
-  ];
+
+  constructor(private http: HttpClient) {
+    
+  }
+
+  photos: Array<any> = [];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
 import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
@@ -10,6 +11,7 @@ import { PhotosModule } from './photos/photos.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     PhotosModule
   ],
   providers: [],
