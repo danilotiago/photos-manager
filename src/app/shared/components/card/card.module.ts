b93df26e5632e276578113aa8bb0f50c02d437e3
danilotiago <programacao.desenvolvimento@gmail.com>
2019-07-16 11:25:45 -0300

    criando card component para receber o html de foto

---
+++ src/app/shared/components/card/card.module.ts
@@ -0,0 +1,10 @@
+import { CardComponent } from './card.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [CardComponent],
+  exports: [CardComponent]
+})
+export class CardModule { }
