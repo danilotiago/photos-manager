218104e5f5ef57bbdd0b3a80280f9cfbb01720ea
danilotiago <programacao.desenvolvimento@gmail.com>
2019-07-16 15:01:14 -0300

    validando form de login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
+
+import { ReactiveFormsModule } from '@angular/forms';
 import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [
+        CommonModule, 
+        ReactiveFormsModule
+    ],
     // components com escopo de pagina nao precisam ser exportados
     declarations: [
         SigninComponent
