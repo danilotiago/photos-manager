c830f3561ef32ad28f4cae9947dd6b8498a3639d
danilotiago <programacao.desenvolvimento@gmail.com>
2019-07-16 15:44:07 -0300

    criando componente de mensagem de input

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { SigninComponent } from './signin/signin.component';
+import { InputMessageModule } from "../shared/components/input-message/input-message.module";
 
 @NgModule({
     imports: [
         CommonModule, 
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        InputMessageModule
     ],
     // components com escopo de pagina nao precisam ser exportados
     declarations: [
---
+++ src/app/shared/components/input-message/input-message.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { InputMessageComponent } from "./input-message.component";
+
+@NgModule({
+    imports: [CommonModule],
+    declarations: [InputMessageComponent],
+    exports: [InputMessageComponent]
+})
+export class InputMessageModule { }
