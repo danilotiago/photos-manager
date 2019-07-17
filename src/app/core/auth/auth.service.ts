166f6ccc939fc94274b7c0f4c023a6ca9154914a
danilotiago <programacao.desenvolvimento@gmail.com>
2019-07-16 21:50:24 -0300

    obtendo token de acesso apos login

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 const API_URL: string = 'http://localhost:3000';
 
@@ -12,6 +13,16 @@ export class AuthService {
     constructor(private http: HttpClient) { }
 
     authenticate(userName: string, password: string): Observable<Object> {
-        return this.http.post(`${API_URL}/user/login`, {userName, password});
+        return this.http
+            .post(`${API_URL}/user/login`, 
+                {userName, password}, 
+                {observe: 'response'}
+            )
+            // executa o tap antes de retornar a resp para o inscrito
+            .pipe(tap(res => {
+                // obtem o token da resposta
+                const authToken = res['headers'].get('x-access-token');
+                console.log(`User ${userName} authenticated with token ${authToken}`)
+            }));
     }
 }
