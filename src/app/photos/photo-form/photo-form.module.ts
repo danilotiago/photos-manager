b47c71ea7bc7ade1362209c26b170a4251373fef
danilotiago <programacao.desenvolvimento@gmail.com>
2019-07-23 18:22:52 -0300

    codificando componente do form de photos

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,3 +1,5 @@
+import { InputMessageModule } from './../../shared/components/input-message/input-message.module';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
@@ -8,7 +10,9 @@ import { PhotoFormComponent } from './photo-form.component';
         PhotoFormComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        ReactiveFormsModule,
+        InputMessageModule
     ]
 })
 export class PhotoFormModule { }
