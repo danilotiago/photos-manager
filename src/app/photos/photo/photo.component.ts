1e1ab749acb940a834d07d66f9e6706ec53ab762
danilotiago <programacao.desenvolvimento@gmail.com>
2019-07-30 08:42:34 -0300

    criando setter para inboundproperties

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+const CLOUD = 'http://localhost:3000/imgs/';
+
 @Component({
   selector: 'app-photo',
   templateUrl: './photo.component.html',
@@ -7,8 +9,21 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PhotoComponent implements OnInit {
 
-  @Input() url: string = '';
+  _url: string = '';
+
   @Input() description: string = '';
+  @Input() set url(url: string) {
+    if (! url.startsWith('data')) {
+      this._url = `${CLOUD}${url}`;
+    }
+     else {
+       this._url = url;
+     }
+  }
+
+  get url() {
+    return this._url;
+  }
 
   constructor() { }
 
